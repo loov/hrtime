b30add0782bcf949e2a8ca7260b2b22a7b8a2786
Egon Elbre <egonelbre@gmail.com>
2019-02-18 11:27:39 +0200

    Add example into package documentation.

--- init.go
+++ init.go
@@ -1,4 +1,23 @@
 // Package hrtime implements High-Resolution Timing functions for benchmarking.
+//
+// The basic usage of this package looks like:
+//
+//     package main
+//
+//     import (
+//         "fmt"
+//         "github.com/loov/hrtime"
+//     )
+//
+//     func main() {
+//         bench := hrtime.NewBenchmark(4 << 10)
+//         for bench.Next() {
+//             time.Sleep(10)
+//         }
+//         fmt.Println(bench.Histogram(10))
+//     }
+//
+// To see more complex refer to the _example folder.
 package hrtime
 
 const calibrationCalls = 1 << 10
