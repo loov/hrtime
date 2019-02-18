607a62a828fc42ccb842503aa8b2e75a845a3956
Egon Elbre <egonelbre@gmail.com>
2019-02-18 11:34:44 +0200

    improve comments about tsc support

--- tsc_amd64.go
+++ tsc_amd64.go
@@ -9,7 +9,13 @@ func initCPU() {
 }
 
 // RDTSCP returns Read Time-Stamp Counter value using RDTSCP asm instruction.
+//
+// If a platform doesn't support the instruction it returns 0.
+// Use TSCSupported to check.
 func RDTSCP() uint64 { return rdtscpAsm() }
 
 // RDTSC returns Read Time-Stamp Counter value using RDTSC asm instruction.
+//
+// If a platform doesn't support the instruction it returns 0.
+// Use TSCSupported to check.
 func RDTSC() uint64 { return rdtscAsm() }
--- tsc_other.go
+++ tsc_other.go
@@ -9,7 +9,13 @@ func initCPU() {
 }
 
 // RDTSCP returns 0 for unsupported configuration
+//
+// If a given OS doesn't support the instruction it returns 0.
+// Use TSCSupported to check.
 func RDTSCP() uint64 { return 0 }
 
 // RDTSC returns 0 for unsupported configuration
+//
+// If a given OS doesn't support the instruction it returns 0.
+// Use TSCSupported to check.
 func RDTSC() uint64 { return 0 }
