ae1960716b2a03bd1849797f58480b0dad8ae88a
Egon Elbre <egonelbre@gmail.com>
2019-04-02 15:04:11 +0300

    hrplot: add clipping logic

--- hrplot/go.mod
+++ hrplot/go.mod
@@ -3,6 +3,6 @@ module github.com/loov/hrtime/hrplot
 go 1.12
 
 require (
-	github.com/loov/hrtime v0.0.0-20190402112844-5ed54104dd45
+	github.com/loov/hrtime v0.0.0-20190402113035-f4bb847046c0
 	github.com/loov/plot v0.0.0-20190402091651-f4b09b520b03
 )
--- hrplot/plot.go
+++ hrplot/plot.go
@@ -5,13 +5,10 @@ package hrplot
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
+	"sort"
 
 	"github.com/loov/plot"
-)
-
-const (
-	defaultWidth  = 800
-	defaultHeight = 300
 )
 
 // Benchmark declares interface for benchmarks it can plot.
@@ -22,7 +19,46 @@ type Benchmark interface {
 }
 
 // Option is for declaring options to plotting.
-type Option interface{ apply() }
+type Option interface{ apply(*plotOptions) }
+
+type plotOptions struct {
+	Width  float64
+	Height float64
+
+	LineClip       float64
+	DensityClip    float64
+	PercentileClip float64
+}
+
+func applyAll(opts ...Option) plotOptions {
+	result := defaultOptions
+	for _, opt := range opts {
+		opt.apply(&result)
+	}
+	return result
+}
+
+var defaultOptions = plotOptions{
+	Width:  800,
+	Height: 300,
+
+	LineClip:       0.9999,
+	DensityClip:    0.99,
+	PercentileClip: 0.9995,
+}
+
+type optionFunc func(*plotOptions)
+
+func (fn optionFunc) apply(options *plotOptions) { fn(options) }
+
+// ClipPercentile specifies how to clip the values.
+func ClipPercentile(percentile float64) Option {
+	return optionFunc(func(options *plotOptions) {
+		options.LineClip = percentile
+		options.DensityClip = percentile
+		options.PercentileClip = percentile
+	})
+}
 
 // label formats the label to be used on plots.
 func label(kind string, b Benchmark) string {
@@ -30,80 +66,105 @@ func label(kind string, b Benchmark) string {
 }
 
 // All plots line, density and percentiles plot on a single image.
-func All(svgfile string, b Benchmark, options ...Option) error {
+func All(svgfile string, b Benchmark, opts ...Option) error {
 	measurements := b.Float64s()
 	if len(measurements) == 0 {
 		return nil
 	}
+	options := applyAll(opts...)
 
 	p := plot.New()
 	stack := plot.NewVStack()
 	stack.Margin = plot.R(5, 5, 5, 5)
 	p.Add(stack)
 
-	stack.Add(plot.NewAxisGroup(lineOptimized(b, measurements)...))
-	stack.Add(plot.NewAxisGroup(density(b, measurements)...))
+	line := plot.NewAxisGroup(lineOptimized(b, measurements)...)
+	line.Y.Max = percentile(measurements, options.LineClip)
+	stack.Add(line)
+
+	density := plot.NewAxisGroup(density(b, measurements)...)
+	density.X.Max = percentile(measurements, options.DensityClip)
+	stack.Add(density)
 
 	percentiles := plot.NewAxisGroup(percentiles(b, measurements)...)
 	percentiles.X = plot.NewPercentilesAxis()
+	percentiles.X.Transform = plot.NewPercentileTransform(4)
+	percentiles.Y.Min, percentiles.Y.Max = 0, percentile(measurements, options.PercentileClip)
 	stack.Add(percentiles)
 
-	svg := plot.NewSVG(defaultWidth, defaultHeight*3)
+	svg := plot.NewSVG(options.Width, options.Height*3)
 	p.Draw(svg)
 
 	return ioutil.WriteFile(svgfile, svg.Bytes(), 0755)
 }
 
 // Line draws a line graph in timing order.
-func Line(svgfile string, b Benchmark, options ...Option) error {
+func Line(svgfile string, b Benchmark, opts ...Option) error {
 	measurements := b.Float64s()
 	if len(measurements) == 0 {
 		return nil
 	}
+	options := applyAll(opts...)
 
 	p := plot.New()
 	p.Margin = plot.R(5, 0, 0, 5)
+	p.Y.Max = percentile(measurements, options.LineClip)
 	p.AddGroup(lineOptimized(b, measurements)...)
 
-	svg := plot.NewSVG(defaultWidth, defaultHeight)
+	svg := plot.NewSVG(options.Width, options.Height)
 	p.Draw(svg)
 
 	return ioutil.WriteFile(svgfile, svg.Bytes(), 0755)
 }
 
 // Density draws a density plot out of benchmark measurements.
-func Density(svgfile string, b Benchmark, options ...Option) error {
+func Density(svgfile string, b Benchmark, opts ...Option) error {
 	measurements := b.Float64s()
 	if len(measurements) == 0 {
 		return nil
 	}
+	options := applyAll(opts...)
 
 	p := plot.New()
 	p.Margin = plot.R(5, 0, 0, 5)
+	p.X.Max = percentile(measurements, options.DensityClip)
 	p.AddGroup(density(b, measurements)...)
 
-	svg := plot.NewSVG(defaultWidth, defaultHeight)
+	svg := plot.NewSVG(options.Width, options.Height)
 	p.Draw(svg)
 
 	return ioutil.WriteFile(svgfile, svg.Bytes(), 0755)
 }
 
 // Percentiles draws a percentiles plot out of benchmark measurements.
-func Percentiles(svgfile string, b Benchmark, options ...Option) error {
+func Percentiles(svgfile string, b Benchmark, opts ...Option) error {
 	measurements := b.Float64s()
 	if len(measurements) == 0 {
 		return nil
 	}
+	options := applyAll(opts...)
 
 	p := plot.New()
 	p.Margin = plot.R(5, 0, 0, 5)
 	p.X = plot.NewPercentilesAxis()
+	p.X.Transform = plot.NewPercentileTransform(4)
+	p.Y.Min, p.Y.Max = 0, percentile(measurements, options.PercentileClip)
 	p.AddGroup(percentiles(b, measurements)...)
 
-	svg := plot.NewSVG(defaultWidth, defaultHeight)
+	svg := plot.NewSVG(options.Width, options.Height)
 	p.Draw(svg)
 
 	return ioutil.WriteFile(svgfile, svg.Bytes(), 0755)
+}
+
+func percentile(measurements []float64, p float64) float64 {
+	sorted := append(measurements[:0:0], measurements...)
+	sort.Float64s(sorted)
+	k := int(math.Ceil(p * float64(len(sorted))))
+	if k >= len(sorted) {
+		k = len(sorted) - 1
+	}
+	return sorted[k]
 }
 
 func line(b Benchmark, measurements []float64) []plot.Element {
@@ -120,7 +181,7 @@ func lineOptimized(b Benchmark, measurements []float64) []plot.Element {
 	return []plot.Element{
 		plot.NewGrid(),
 		plot.NewGizmo(),
-		plot.NewOptimizedLine(b.Unit(), plot.Points(nil, measurements), 1),
+		plot.NewOptimizedLine(b.Unit(), plot.Points(nil, measurements), 2),
 		plot.NewTickLabels(),
 		plot.NewTextbox(label("line", b)),
 	}
